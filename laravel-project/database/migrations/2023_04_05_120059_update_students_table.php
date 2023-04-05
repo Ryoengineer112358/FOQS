d90ecc72f11647478ede9a0f9c84792637c3c01a
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-04-05 12:26:52 +0900

    studentsテーブルに学部カラム追加、sexカラムをgenderカラムに変更

---
+++ laravel-project/database/migrations/2023_04_05_120059_update_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->string('first_choice_faculty', 100)->after('first_choice_university');
+            $table->renameColumn('sex', 'gender');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('first_choice_faculty');
+            $table->renameColumn('gender', 'sex');
+    });
+    }
+};
