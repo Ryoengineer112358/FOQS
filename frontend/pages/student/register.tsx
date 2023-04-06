4348082d3bc3a8d0afc8d55785836d8cdbad99dd
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-04-06 12:23:25 +0900

    新規登録画面を中央揃えに変更

--- frontend/pages/student/register.tsx
+++ frontend/pages/student/register.tsx
@@ -51,181 +51,185 @@ const Register: NextPage = () => {
 
   return (
     <DefaultLayout middleware={middleware}>
-      <Card sx={{ p: 4, borderRadius: 8 }}>
+      <Card sx={{ p: 4, mb: 4, borderRadius: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <form onSubmit={submitForm}>
-          {/* 苗字と名前 */}
-          <Box>
-            <Grid container spacing={1}>
-              <Grid item xs={5}>
-                <TextField
-                  id="last_name"
-                  type="text"
-                  value={lastName}
-                  label="苗字"
-                  onChange={(event) => setLastName(event.target.value)}
-                  required
-                  //autoFocus
-                  sx={{ width: "100%" }}
-                />
+          <Grid container justifyContent="center">
+            {/* 苗字と名前 */}
+            <Box>
+              <Grid container spacing={1}>
+                <Grid item xs={6}>
+                  <TextField
+                    id="last_name"
+                    type="text"
+                    value={lastName}
+                    label="苗字"
+                    onChange={(event) => setLastName(event.target.value)}
+                    required
+                    sx={{ width: "100%" }}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    id="first_name"
+                    type="text"
+                    value={firstName}
+                    label="名前"
+                    onChange={(event) => setFirstName(event.target.value)}
+                    required
+                    sx={{ width: "100%" }}
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={5}>
+            </Box>
+
+            {/* Email Address */}
+            <Grid item xs={12}>
+              <Box sx={{ marginTop: "0.8rem" }}>
                 <TextField
-                  id="first_name"
-                  type="text"
-                  value={firstName}
-                  label="名前"
-                  onChange={(event) => setFirstName(event.target.value)}
+                  id="email"
+                  type="email"
+                  value={email}
+                  label="Eメール"
+                  onChange={(event) => setEmail(event.target.value)}
                   required
                   sx={{ width: "100%" }}
                 />
+              </Box>
+
+              {/* Password */}
+              <Box sx={{ marginTop: "0.8rem" }}>
+                <TextField
+                  id="password"
+                  type="password"
+                  value={password}
+                  label="パスワード"
+                  onChange={(event) => setPassword(event.target.value)}
+                  required
+                  sx={{ width: "100%" }}
+                />
+              </Box>
+
+              {/* Password Confirmation */}
+              <Box sx={{ marginTop: "0.8rem" }}>
+                <TextField
+                  id="password_confirmation"
+                  type="password"
+                  value={passwordConfirmation}
+                  label="パスワードの確認"
+                  onChange={(event) =>
+                    setPasswordConfirmation(event.target.value)
+                  }
+                  required
+                  sx={{ width: "100%" }}
+                />
+              </Box>
+
+              {/* 高校名 */}
+              <Box sx={{ marginTop: "0.8rem" }}>
+                <TextField
+                  id="high_school"
+                  type="text"
+                  value={highSchool}
+                  label="高校名"
+                  onChange={(event) => setHighSchool(event.target.value)}
+                  InputProps={{
+                    endAdornment: <InputAdornment position="end">高校</InputAdornment>,
+                  }}
+                  required
+                  sx={{ width: "100%" }}
+                />
+              </Box>
+
+              {/* 第一志望大学 */}
+              <Box sx={{ marginTop: "0.8rem" }}>
+                <TextField
+                  id="first_choice_university"
+                  type="text"
+                  value={firstChoiceUniversity}
+                  label="第一志望大学"
+                  onChange={(event) => setFirstChoiceUniversity(event.target.value)}
+                  InputProps={{
+                    endAdornment: <InputAdornment position="end">大学</InputAdornment>,
+                  }}
+                  required
+                  sx={{ width: "100%" }}
+                />
+              </Box>
+
+              {/* 第一志望学部 */}
+              <Box sx={{ marginTop: "0.8rem" }}>
+                <TextField
+                  id="first_choice_faculty"
+                  type="text"
+                  value={firstChoiceFaculty}
+                  label="第一志望学部"
+                  onChange={(event) => setFirstChoiceFaculty(event.target.value)}
+                  InputProps={{
+                    endAdornment: <InputAdornment position="end">学部</InputAdornment>,
+                  }}
+                  required
+                  sx={{ width: "100%" }}
+                />
+              </Box>
+
+              {/* 生年月日 */}
+              <Box sx={{ marginTop: "0.8rem" }}>
+                <TextField
+                  id="birth_date"
+                  type="date"
+                  value={birthDate}
+                  label={birthDate ? "生年月日" : ""}
+                  onChange={(event) => setBirthDate(event.target.value)}
+                  required
+                  sx={{ width: "100%" }}
+                  InputLabelProps={{
+                    shrink: !!birthDate,
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        {!birthDate && "生年月日"}
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Box>
+
+              {/* 性別 */}
+              <Grid container justifyContent="center" sx={{ marginTop: "0.8rem" }}>
+                <Grid item xs={5}>
+                  <FormControl fullWidth>
+                    <InputLabel id="gender-label" htmlFor="gender">性別</InputLabel>
+                    <Select
+                      labelId="gender-label"
+                      id="gender"
+                      value={gender}
+                      onChange={(event) => setGender(event.target.value)}
+                      required
+                      label="性別"
+                    >
+                      <MenuItem value={0}>男</MenuItem>
+                      <MenuItem value={1}>女</MenuItem>
+                      <MenuItem value={2}>無回答</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
               </Grid>
             </Grid>
 
-            {/* Email Address */}
-            <Box sx={{ marginTop: "0.8rem" }}>
-              <TextField
-                id="email"
-                type="email"
-                value={email}
-                label="Eメール"
-                onChange={(event) => setEmail(event.target.value)}
-                required
-                sx={{ width: "80%" }}
-              />
-            </Box>
-
-            {/* Password */}
-            <Box sx={{ marginTop: "0.8rem" }}>
-              <TextField
-                id="password"
-                type="password"
-                value={password}
-                label="パスワード"
-                onChange={(event) => setPassword(event.target.value)}
-                required
-                sx={{ width: "80%" }}
-              />
-            </Box>
-
-            {/* Password Confirmation */}
-            <Box sx={{ marginTop: "0.8rem" }}>
-              <TextField
-                id="password_confirmation"
-                type="password"
-                value={passwordConfirmation}
-                label="パスワードの確認"
-                onChange={(event) =>
-                  setPasswordConfirmation(event.target.value)
-                }
-                required
-                sx={{ width: "80%" }}
-              />
-            </Box>
-
-            {/* 高校名 */}
-            <Box sx={{ marginTop: "0.8rem" }}>
-              <TextField
-                id="high_school"
-                type="text"
-                value={highSchool}
-                label="高校名"
-                onChange={(event) => setHighSchool(event.target.value)}
-                InputProps={{
-                  endAdornment: <InputAdornment position="end">高校</InputAdornment>,
-                }}
-                required
-                sx={{ width: "80%" }}
-              />
-            </Box>
-
-            {/* 第一志望大学 */}
-            <Box sx={{ marginTop: "0.8rem" }}>
-              <TextField
-                id="first_choice_university"
-                type="text"
-                value={firstChoiceUniversity}
-                label="第一志望大学"
-                onChange={(event) => setFirstChoiceUniversity(event.target.value)}
-                InputProps={{
-                  endAdornment: <InputAdornment position="end">大学</InputAdornment>,
-                }}
-                required
-                sx={{ width: "80%" }}
-              />
-            </Box>
-
-            {/* 第一志望学部 */}
-            <Box sx={{ marginTop: "0.8rem" }}>
-              <TextField
-                id="first_choice_faculty"
-                type="text"
-                value={firstChoiceFaculty}
-                label="第一志望学部"
-                onChange={(event) => setFirstChoiceFaculty(event.target.value)}
-                InputProps={{
-                  endAdornment: <InputAdornment position="end">学部</InputAdornment>,
-                }}
-                required
-                sx={{ width: "80%" }}
-              />
-            </Box>
-
-            {/* 生年月日 */}
-            <Box sx={{ marginTop: "0.8rem" }}>
-              <TextField
-                id="birth_date"
-                type="date"
-                value={birthDate}
-                label={birthDate ? "生年月日" : ""}
-                onChange={(event) => setBirthDate(event.target.value)}
-                required
-                sx={{ width: "80%" }}
-                InputLabelProps={{
-                  shrink: !!birthDate,
-                }}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      {!birthDate && "生年月日"}
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Box>
-
-            {/* 性別 */}
-            <Box sx={{ marginTop: "0.8rem" }}>
-              <FormControl fullWidth sx={{ width: "80%" }}>
-                <InputLabel id="gender-label" htmlFor="gender">性別</InputLabel>
-                <Select
-                  labelId="gender-label"
-                  id="gender"
-                  value={gender}
-                  onChange={(event) => setGender(event.target.value)}
-                  required
-                  label="性別"
-                >
-                  <MenuItem value={0}>男</MenuItem>
-                  <MenuItem value={1}>女</MenuItem>
-                  <MenuItem value={2}>無回答</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-
-
-            <Container sx={{ marginTop: "0.8rem" }}>
+            <Grid item xs={12} sx={{marginTop: '0.8rem'}}>
               <Button
                 type="submit"
                 variant="contained"
-                sx={{fontSize: '1.2rem', py: '0.8rem', px: '2.4rem'}}
+                sx={{ fontSize: '1.2rem', py: '0.8rem', px: '2.4rem', width: '100%' }}
               >
                 登録
               </Button>
-            </Container>
-            <Container sx={{ marginTop: "0.8rem" }}>
+            </Grid>
+            <Container sx={{ marginTop: "1.6rem", textAlign: "center" }}>
               <Link href="/student/login">ログインページに戻る</Link>
             </Container>
-          </Box>
+          </Grid>
         </form>
       </Card>
     </DefaultLayout>
@@ -233,3 +237,4 @@ const Register: NextPage = () => {
 };
 
 export default Register;
+
