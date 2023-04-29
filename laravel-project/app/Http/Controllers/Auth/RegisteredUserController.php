5f87968c9e78e2da6e0f20b0a353c2d7bd9303b0
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-04-29 21:53:24 +0900

    生徒講師両方ユーザー登録できるようにコントローラ, モデルを修正

--- laravel-project/app/Http/Controllers/Auth/RegisteredUserController.php
+++ laravel-project/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
+use App\Models\Tutor;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -11,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Symfony\Component\HttpFoundation\Response;
 
 class RegisteredUserController extends Controller
 {
@@ -24,29 +26,57 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
+        $userType = $request->input('user_type');
+
         $request->validate([
             'last_name' => 'required|string|max:255',
             'first_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:students|unique:tutors',
             'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
-            'high_school' => 'required|string|max:255',
-            'first_choice_university' => 'required|string|max:255',
-            'first_choice_faculty' => 'required|string|max:255',
             'birth_date' => 'required|date',
             'gender' => 'required|integer|min:0|max:2',
         ]);
 
-        $user = Student::create([
-            'last_name' => $request->last_name,
-            'first_name' => $request->first_name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'high_school' => $request->high_school,
-            'first_choice_university' => $request->first_choice_university,
-            'first_choice_faculty' => $request->first_choice_faculty,
-            'birth_date' => $request->birth_date,
-            'gender' => $request->gender,
-        ]);
+        switch ($userType) {
+            case 'student':
+                $request->validate([
+                    'high_school' => 'required|string|max:255',
+                    'first_choice_university' => 'required|string|max:255',
+                    'first_choice_faculty' => 'required|string|max:255',
+                ]);
+                $user = Student::create([
+                    'last_name' => $request->last_name,
+                    'first_name' => $request->first_name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                    'high_school' => $request->high_school,
+                    'first_choice_university' => $request->first_choice_university,
+                    'first_choice_faculty' => $request->first_choice_faculty,
+                    'birth_date' => $request->birth_date,
+                    'gender' => $request->gender,
+                ]);
+                break;
+
+            case 'tutor':
+                $request->validate([
+                    'university' => 'required|string|max:255',
+                    'faculty' => 'required|string|max:255',
+                ]);
+                $user = Tutor::create([
+                    'last_name' => $request->last_name,
+                    'first_name' => $request->first_name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                    'university' => $request->university,
+                    'faculty' => $request->faculty,
+                    'birth_date' => $request->birth_date,
+                    'gender' => $request->gender,
+                ]);
+                break;
+
+            default:
+                return response()->json(['error' => 'Invalid user type'], Response::HTTP_BAD_REQUEST);
+        }
 
         event(new Registered($user));
 
--- laravel-project/app/Models/Tutor.php
+++ laravel-project/app/Models/Tutor.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use App\Notifications\ResetPasswordNotification;
 
 class Tutor extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
@@ -30,5 +32,10 @@ class Tutor extends Authenticatable implements MustVerifyEmail
     public function student_comments()
     {
         return $this->hasMany(StudentComment::class);
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token, 'tutor'));
     }
 }
