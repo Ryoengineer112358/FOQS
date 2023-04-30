ff7b2cd758af0fb9cdc8e4e584f5136e21d9edd8
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-05-01 03:03:20 +0900

    パスワードリセットリンク送信処理の共通化

--- laravel-project/app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ laravel-project/app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Rule;
 
 class PasswordResetLinkController extends Controller
 {
@@ -19,14 +20,16 @@ class PasswordResetLinkController extends Controller
      */
     public function store(Request $request)
     {
+        $userType = $request->input('user_type');
         $request->validate([
-            'email' => ['required', 'email', 'exists:students,email'],
+            'email' => ['required', 'email', Rule::exists($userType . 's', 'email')],
+            'user_type' => ['required', Rule::in(['student', 'tutor'])],
         ]);
 
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
+        $status = Password::broker($userType . 's')->sendResetLink(
             $request->only('email')
         );
 
