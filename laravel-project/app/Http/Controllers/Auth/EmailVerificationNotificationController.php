677ec23560c7c9fe88920ff27ae5fb0eb3c9bdcf
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-04-30 16:26:44 +0900

    認証メール再送信のためのコントローラを動的に対応させる変更

--- laravel-project/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ laravel-project/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class EmailVerificationNotificationController extends Controller
 {
@@ -16,8 +17,10 @@ class EmailVerificationNotificationController extends Controller
      */
     public function store(Request $request)
     {
+        $userType = $request->input('user_type');
+
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME);
+            return redirect()->intended(config('app.frontend_url').'/'.$userType);
         }
 
         $request->user()->sendEmailVerificationNotification();
