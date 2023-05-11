ef49769e42cc3b4b2d3c7476b1ac67e1f92454d0
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-05-12 03:06:23 +0900

    convertDateTypeOnObject関数を、ネストされた'_at'キーも適切に処理するように変更

--- frontend/utils.ts
+++ frontend/utils.ts
@@ -1,11 +1,20 @@
 import { GetServerSideProps } from "next";
 
-export function convertDateTypeOnObject(obj: any) {
-  for (const key in obj) {
-    const value = obj[key]
-    if (value && typeof value == "string" && key.indexOf("_at") >= 0) {
-      obj[key] = new Date(value)
+export function convertDateTypeOnObject(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map(convertDateTypeOnObject);
+  } else if (obj !== null && typeof obj === 'object') {
+    const result: { [key: string]: any } = {}; // ここを変更
+    for (const key in obj) {
+      const value = obj[key];
+      if (value && typeof value == "string" && key.indexOf("_at") >= 0) {
+        result[key] = new Date(value);
+      } else {
+        result[key] = convertDateTypeOnObject(value);
+      }
     }
+    return result;
+  } else {
+    return obj;
   }
-  return obj
 }
