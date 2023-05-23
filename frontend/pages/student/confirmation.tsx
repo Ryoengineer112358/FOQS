623064b64751d650359afbf62fccf72b473cc8ee
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-05-23 16:16:19 +0900

    確認画面の新規ボタンの処理を実装

--- frontend/pages/student/confirmation.tsx
+++ frontend/pages/student/confirmation.tsx
@@ -64,10 +64,10 @@ const Confirmation: NextPage = () => {
             <BackButton />
           </Grid>
           <Grid item xs={4} md={2}>
-            <MiddleButton text={"質問を変更する"} href={"../question"} />
+            <MiddleButton text='質問を変更する' href='question' />
           </Grid>
           <Grid item xs={4} md={2}>
-            <MiddleButton text={"講師を変更する"} href={"../tutor-option"} />
+            <MiddleButton text='講師を変更する' href='tutor-option' />
           </Grid>
         </Grid>
         <Grid container justifyContent="center" marginTop={2} marginBottom={3}>
