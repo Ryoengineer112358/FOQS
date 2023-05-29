13ae6742bffdcd2cf83f440e302976e256c092c4
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-05-30 03:04:30 +0900

    講師トップページのUI変更

--- frontend/pages/tutor/index.tsx
+++ frontend/pages/tutor/index.tsx
@@ -9,7 +9,7 @@ const Top: NextPage = () => {
   return (
     <>
       <Home middleware={"tutor"} />
-      <Grid container justifyContent="center" marginTop={2} marginBottom={3}>
+      <Grid container justifyContent="center" marginTop={3} marginBottom={3}>
         <Grid item xs={8} sm={6} md={4}>
           <MiddleButton text="質問を見つける" href="tutor/unassigned-questions"/>
         </Grid>
