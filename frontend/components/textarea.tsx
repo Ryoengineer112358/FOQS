e41691a7b961e2f134b146b6a29747bd9cf6a279
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-05-30 21:20:08 +0900

    生徒質問入力画面に画像貼り付け機能と写真撮影機能を実装

--- frontend/components/textarea.tsx
+++ frontend/components/textarea.tsx
@@ -1,6 +1,7 @@
-import {TextareaAutosize} from "@mui/material";
+import { TextareaAutosize, Box } from "@mui/material";
 import { useRef } from "react";
-import ImageIcon from '@mui/icons-material/Image';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
 
 type Props = {
   value: string;
@@ -8,31 +9,51 @@ type Props = {
 }
 
 const Textarea = (props: Props) => {
-
+  const cameraRef = useRef<HTMLInputElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <>
-    <TextareaAutosize
-      maxRows={100}
-      placeholder="ここに質問を入力してください"
-      onChange={(e: any) => props.changeHandler(e.target.value)}
-      value={props.value}
-      style={{
-        padding: 10,
-        width: "100%",
-        height: 480,
-        borderRadius: 30
-      }}
-    />
-    <ImageIcon
-      onClick={() => {
-        inputRef.current?.click();
-      }
-      }
-    />
-    <input type='file' hidden ref={inputRef} accept='image/*' capture='user' multiple />
-    </>
+    <Box position="relative" width="100%">
+      <TextareaAutosize
+        maxRows={100}
+        placeholder="ここに質問を入力してください"
+        onChange={(e: any) => props.changeHandler(e.target.value)}
+        value={props.value}
+        style={{
+          fontSize: 16,
+          padding: 10,
+          width: "100%",
+          height: 480,
+          borderRadius: 30
+        }}
+      />
+      <CameraAltOutlinedIcon
+        onClick={() => {
+          cameraRef.current?.click();
+        }}
+        style={{
+          color: "gray",
+          fontSize: 80,
+          position: "absolute",
+          bottom: 15,
+          right: 15,
+        }}
+      />
+      <input type='file' hidden ref={cameraRef} accept='image/*' capture='environment' multiple />
+      <ImageOutlinedIcon
+        onClick={() => {
+          inputRef.current?.click();
+        }}
+        style={{
+          color: "gray",
+          fontSize: 80,
+          position: "absolute",
+          bottom: 15,
+          right: 95,
+        }}
+      />
+      <input type='file' hidden ref={inputRef} accept='image/*' multiple />
+    </Box>
   )
 }
 
