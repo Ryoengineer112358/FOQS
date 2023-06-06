80a544f48161f9fd7c5bdfe1d0e0e340c2136a1d
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-06-06 19:11:32 +0900

    トップページの共通化

--- frontend/pages/student/index.tsx
+++ frontend/pages/student/index.tsx
@@ -1,22 +1,12 @@
 import type { NextPage } from 'next'
-import {Grid} from "@mui/material";
 import * as React from "react";
 import Home from "@/components/Pages/Home";
-import MiddleButton from "@/components/MiddleButton";
 
 const Top: NextPage = () => {
 
   return (
     <>
       <Home middleware={"student"} />
-      <Grid container justifyContent="center" spacing={1} marginTop={2} marginBottom={3}>
-        <Grid item xs={6} sm={4} md={3}>
-          <MiddleButton text="質問履歴" href="student/question-history"/>
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <MiddleButton text="質問する" href={"student/create-question"}/>
-        </Grid>
-      </Grid>
     </>
   )
 }
--- frontend/pages/tutor/index.tsx
+++ frontend/pages/tutor/index.tsx
@@ -1,19 +1,12 @@
 import type { NextPage } from 'next'
-import {Grid} from "@mui/material";
 import * as React from "react";
 import Home from "@/components/Pages/Home";
-import MiddleButton from "@/components/MiddleButton";
 
 const Top: NextPage = () => {
 
   return (
     <>
       <Home middleware={"tutor"} />
-      <Grid container justifyContent="center" marginTop={3} marginBottom={3}>
-        <Grid item xs={8} sm={6} md={4}>
-          <MiddleButton text="質問を見つける" href="tutor/unassigned-questions"/>
-        </Grid>
-      </Grid>
     </>
 
   )
