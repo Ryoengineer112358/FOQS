cc89129aa3341b68f16d859f07b029bf62674926
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-06-06 23:50:31 +0900

    質問履歴の共通化

---
+++ frontend/components/pages/QuestionHistory.tsx
@@ -0,0 +1,74 @@
+import { useAuth } from '@/hooks/auth';
+import {Grid} from "@mui/material";
+import DefaultLayout from "@/components/DefaultLayout";
+import CardMessage from "@/components/CardMessage";
+import {useEffect, useState} from "react";
+import {StudentQuestion, Middleware} from "@/types";
+import axios from "@/lib/axios";
+import MiddleButton from '@/components/MiddleButton';
+import { motion, AnimatePresence } from 'framer-motion';
+
+type Props = {
+	middleware: Middleware;
+}
+
+const QuestionHistory = (props: Props) => {
+  const middleware = props.middleware;
+  const { user } = useAuth({ middleware: middleware })
+  const [questions, setQuestions] = useState<Array<StudentQuestion>>([]);
+  const [animationStart, setAnimationStart] = useState<boolean>(false);
+
+  useEffect(() => {
+    axios.get<StudentQuestion[]>('/api/questions?solved_only=true').then(
+      result => {
+        setQuestions(result.data)
+        setAnimationStart(true);
+      }
+    )
+  }, [])
+
+  return (
+    <>
+      <DefaultLayout middleware={middleware}>
+        <div></div>
+      </DefaultLayout>
+      <Grid container justifyContent="center" style={{ overflow: 'hidden' }}>
+        <Grid item xs={12} md={8}>
+          {questions.map((x, index) =>
+            <motion.div 
+              initial={{ opacity: 0, x: '100vw' }}
+              animate={{ opacity: 1, x: 0 }} 
+              transition={{ duration: 0.1, delay: index * 0.1, ease: "easeOut" }}
+              key={x.id}
+            >
+              <CardMessage text={x.text} href={`/${middleware}/chat/${x.id}`} />
+            </motion.div>
+          )}
+        </Grid>
+      </Grid>
+      <AnimatePresence>
+        {animationStart && (
+          <Grid container justifyContent="center">
+            <Grid item xs={6} sm={4} md={3} marginTop={2} marginBottom={3}>
+              <motion.div 
+                initial={{ opacity: 0, x: '100vw' }}
+                animate={{ opacity: 1, x: 0 }} 
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 12,
+                  delay: questions.length * 0.1 - 0.1,
+                }}
+                exit={{ opacity: 0 }}
+              >
+                <MiddleButton text="ホームに戻る" href={`/${middleware}`} />
+              </motion.div>
+            </Grid>
+          </Grid>
+        )}
+      </AnimatePresence>
+    </>
+  )
+}
+
+export default QuestionHistory
--- frontend/pages/tutor/question-history.tsx
+++ frontend/pages/tutor/question-history.tsx
@@ -1,34 +1,8 @@
-import { useAuth } from '@/hooks/auth'
 import type { NextPage } from 'next'
-import {Grid} from "@mui/material";
-import * as React from "react";
-import DefaultLayout from "@/components/DefaultLayout";
-import CardMessage from "@/components/CardMessage";
-import HomeButton from "@/components/HomeButton";
+import * as QuestionHistoryComponent from '@/components/Pages/QuestionHistory'
 
 const QuestionHistory: NextPage = () => {
-  const middleware = "tutor"
-  const { user } = useAuth({ middleware: middleware })
-
-  return (
-    <>
-      <DefaultLayout middleware={middleware}>
-        <div></div>
-      </DefaultLayout>
-      <Grid container justifyContent="center">
-        <Grid xs={12}>
-          <CardMessage text="画像の問題について質問です" href="/"/>
-          <CardMessage text="画像の問題について質問です" href={""}/>
-          <CardMessage text="画像の問題について質問です" href={""}/>
-          <CardMessage text="画像の問題について質問です" href={""}/>
-          <CardMessage text="画像の問題について質問です" href={""}/>
-        </Grid>
-      </Grid>
-      <Grid container justifyContent='right'>
-        <HomeButton href='/tutor' />
-      </Grid>
-    </>
-  )
+  return <QuestionHistoryComponent.default middleware="tutor" />
 }
 
 export default QuestionHistory
