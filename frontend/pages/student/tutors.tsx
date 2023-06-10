e219f53f8f6f3343bcb52876e2a15f09140a6909
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-06-10 23:51:22 +0900

    講師一覧画面にアニメーションを実装

--- frontend/pages/student/tutors.tsx
+++ frontend/pages/student/tutors.tsx
@@ -7,28 +7,60 @@ import DefaultLayout from "@/components/DefaultLayout";
 import BackButton from "@/components/BackButton";
 import Tutor from "@/components/Tutor";
 import {State} from "@/store"
+import { motion, AnimatePresence } from "framer-motion"
+import { useState, useEffect } from "react"
 
 const Tutors: NextPage = () => {
   const middleware = "student"
   const { user } = useAuth({ middleware: middleware })
   const tutors = useSelector((state: State) => state.tutors)
+  const [animationStart, setAnimationStart] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (tutors.length > 0) {
+      setAnimationStart(true);
+    }
+  }, [tutors]);
 
   return (
     <>
       <DefaultLayout middleware={middleware}>
         <div></div>
       </DefaultLayout>
-      <Grid container justifyContent="center">
-        <Grid item xs={12} md={8}>
-          {tutors.map(t =>
-            <Tutor key={t.id} text={`${t.university}${t.faculty} ${t.last_name}先生`} href={`tutor-profile/${t.id}`} />
+      <Grid container justifyContent="center" style={{ overflow: 'hidden' }}>
+        <Grid item xs={12} sm={10} md={8}>
+          {tutors.map((t, index) =>
+            <motion.div
+              initial={{ opacity: 0, x: '100vw' }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.1, delay: index * 0.1, ease: "easeOut" }}
+              key={t.id}
+            >
+              <Tutor key={t.id} text={`${t.university}${t.faculty} ${t.last_name}先生`} href={`tutor-profile/${t.id}`} />
+            </motion.div>
           )}
         </Grid>
-      </Grid>
-      <Grid container justifyContent="center" marginTop={3} marginBottom={3}>
-        <Grid item xs={6} md={4}>
-          <BackButton />
-        </Grid>
+        <AnimatePresence>
+          {animationStart && (
+            <Grid container justifyContent="center" marginTop={3} marginBottom={3}>
+              <Grid item xs={6} sm={4} md={3}>
+                <motion.div
+                  initial={{ opacity: 0, x: '100vw' }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 100,
+                    damping: 12,
+                    delay: tutors.length * 0.1 - 0.1,
+                  }}
+                  exit={{ opacity: 0 }}
+                >
+                  <BackButton />
+                </motion.div>
+              </Grid>
+            </Grid>
+          )}
+        </AnimatePresence>
       </Grid>
     </>
   )
