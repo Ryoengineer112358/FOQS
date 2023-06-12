9d97fa813008da99e42a5d9b0a17281f62619e4b
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-06-12 18:41:09 +0900

    リファクタリング：MyPageコンポーネントの内容をMyPageページコンポーネントに統合し、MyPageコンポーネントを削除

--- frontend/pages/tutor/my-page.tsx
+++ frontend/pages/tutor/my-page.tsx
@@ -1,8 +1,30 @@
+import { useAuth } from '@/hooks/auth'
+import UserInfo from '@/components/UserInfo'
+import DefaultLayout from '@/components/DefaultLayout'
+import { Grid } from "@mui/material"
+import LinkButton from '@/components/LinkButton'
 import type { NextPage } from 'next'
-import * as MyPapeComponent from '@/components/Pages/MyPage'
 
 const MyPage: NextPage = () => {
-  return <MyPapeComponent.default middleware="tutor" href="/tutor" />
+  const { user } = useAuth({ middleware: "tutor" })
+
+  return (
+    <>
+      <DefaultLayout middleware="tutor">
+        <div></div>
+      </DefaultLayout>
+      <Grid container justifyContent="center">
+        <Grid item xs={12} sm={6} md={4}>
+          {user && <UserInfo {...user}/>}
+        </Grid>
+      </Grid>
+      <Grid container justifyContent='center'>
+        <Grid item xs={6} sm={4} md={3} marginTop={2} marginBottom={3}>
+          <LinkButton text='ホームに戻る' href="/tutor" />
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
 export default MyPage
