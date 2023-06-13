0f06c45a4a127409693b4c2ccf77691baf8dd38f
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-06-13 17:01:37 +0900

    「講師ステータス」を表示するための新規コンポーネントを作成し、評価表示を追加

---
+++ frontend/components/TutorStatus.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import { Typography } from '@mui/material';
+import axios from "@/lib/axios";
+
+type Props = {
+    tutorId: number;
+}
+
+const TutorStatus = (props: Props) => {
+    const [averageRating, setAverageRating] = useState<number | null |undefined>(undefined)
+  
+  useEffect(() => {
+    if (props.tutorId) {
+      axios.get(`/api/tutors/${props.tutorId}/average-rating`)
+        .then(response => setAverageRating(response.data.average_rating))
+        .catch(() => setAverageRating(undefined))
+    }
+  }, [props.tutorId]);
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        p: 2,
+        backgroundColor: 'primary.main',
+        borderRadius: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          marginTop: 0,
+        }}
+      >
+        <Typography>評価：</Typography>
+          {averageRating === undefined ? (
+            <Typography>集計中…</Typography>
+            ) : (
+            averageRating !== null ? (
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Rating name='average-rating' value={averageRating} readOnly size="small" />
+                <Typography sx={{ marginLeft: 1 }}>
+                  {parseFloat(Number(averageRating).toFixed(2)).toString()}/5
+                </Typography>
+              </Box>
+            ) : (
+              <Typography>まだ評価がありません</Typography>
+            )
+        )}
+      </Box>
+    </Box>
+  );
+}
+
+export default TutorStatus;
--- frontend/pages/tutor/my-page.tsx
+++ frontend/pages/tutor/my-page.tsx
@@ -1,9 +1,10 @@
 import { useAuth } from '@/hooks/auth'
 import UserInfo from '@/components/UserInfo'
 import DefaultLayout from '@/components/DefaultLayout'
-import { Grid } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 import LinkButton from '@/components/LinkButton'
 import type { NextPage } from 'next'
+import TutorStatus from '@/components/TutorStatus'
 
 const MyPage: NextPage = () => {
   const { user } = useAuth({ middleware: "tutor" })
@@ -13,9 +14,34 @@ const MyPage: NextPage = () => {
       <DefaultLayout middleware="tutor">
         <div></div>
       </DefaultLayout>
-      <Grid container justifyContent="center">
-        <Grid item xs={12} sm={6} md={4}>
-          {user && <UserInfo {...user}/>}
+      <Grid container justifyContent="center" marginBottom={1}>
+        <Grid item xs={11} sm={7} md={5}>
+          {user &&
+            <>
+              <Typography
+                variant="h4"
+                sx={{ color: 'white', textAlign: 'center' }}
+              >
+                講師ステータス
+              </Typography> 
+              <TutorStatus tutorId={user.id} />
+            </>
+          }
+        </Grid>
+      </Grid>
+      <Grid container justifyContent="center" marginTop={2}>
+        <Grid item xs={11} sm={7} md={5}>
+          {user && 
+            <>
+              <Typography
+                variant="h4"
+                sx={{ color: 'white', textAlign: 'center' }}
+              >
+                ユーザー情報
+              </Typography>
+              <UserInfo {...user}/>
+            </>
+          }
         </Grid>
       </Grid>
       <Grid container justifyContent='center'>
