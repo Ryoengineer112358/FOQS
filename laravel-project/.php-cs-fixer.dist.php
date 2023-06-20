8d056a58681951277ccb470ba3f9852260b5326a
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-06-21 01:28:59 +0900

    バックエンドでPHP-CS-FixerをLinter/Formatterとして導入

---
+++ laravel-project/.php-cs-fixer.dist.php
@@ -0,0 +1,35 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude(['vendor', 'storage', 'bootstrap/cache', 'tests'])
+    ->notPath('_ide_helper.php')
+    ->notPath('storage/framework/views/*');
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => [
+            'default' => 'single_space',
+            'operators' => ['=>' => null, '=' => 'single_space']
+        ],
+        'cast_spaces' => ['space' => 'single'],
+        'concat_space' => ['spacing' => 'one'],
+        'declare_equal_normalize' => ['space' => 'none'],
+        'function_typehint_space' => true,
+        'include' => true,
+        'new_with_braces' => true,
+        'no_blank_lines_after_class_opening' => true,
+        'no_leading_import_slash' => true,
+        'no_whitespace_in_blank_line' => true,
+        'normalize_index_brace' => true,
+        'object_operator_without_whitespace' => true,
+        'single_quote' => true,
+        'ternary_operator_spaces' => true,
+        'trailing_comma_in_multiline' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder);
