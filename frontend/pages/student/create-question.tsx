44ca08873710c8965164fb832fd19ace9cf27084
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-07-04 01:38:27 +0900

    CreateQuestionページコンポーネントとQuestionInputAreaコンポーネントを統合

--- frontend/pages/student/create-question.tsx
+++ frontend/pages/student/create-question.tsx
@@ -1,9 +1,14 @@
 import { useAuth } from '@/hooks/auth'
 import type { NextPage } from 'next'
-import { Grid } from '@mui/material'
+import { Grid, TextareaAutosize, Box, IconButton } from '@mui/material'
+import { useRef } from 'react'
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined'
+import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined'
+import CloseIcon from '@mui/icons-material/Close'
+import { removeImage } from '@/store/modules/newQuestion'
+import { unwrapResult } from '@reduxjs/toolkit'
 import DefaultLayout from '@/components/DefaultLayout'
 import BackButton from '@/components/BackButton'
-import QuestionInputArea from '@/components/QuestionInputArea'
 import LinkButton from '@/components/LinkButton'
 import { useAppDispatch, State } from '@/store'
 import { setText, setImages } from '@/store/modules/newQuestion'
@@ -14,6 +19,8 @@ const CreateQuestion: NextPage = () => {
   const { user } = useAuth({ middleware: middleware })
   const newQuestion = useSelector((state: State) => state.newQuestion)
   const dispatch = useAppDispatch()
+  const cameraRef = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const onChangeQuestionContent = (value: string) => {
     dispatch(setText(value))
@@ -23,6 +30,47 @@ const CreateQuestion: NextPage = () => {
     dispatch(setImages(value))
   }
 
+  const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const files = Array.from(e.target.files)
+      const objectUrls = [] as string[]
+
+      for (const file of files) {
+        if (file.type === 'image/heic' || file.type === 'image/heif') {
+          try {
+            // ダイナミックインポート
+            const heic2any = (await import('heic2any')).default
+            const convertedBlob = (await heic2any({
+              blob: file,
+              toType: 'image/jpeg',
+              quality: 0.8,
+            })) as Blob
+            objectUrls.push(URL.createObjectURL(convertedBlob))
+          } catch (err) {
+            console.error('Failed to convert HEIC/HEIF to JPEG: ', err)
+          }
+        } else if (file.type.match('image.*')) {
+          objectUrls.push(URL.createObjectURL(file))
+        }
+      }
+
+      onChangeQuestionImages([...(newQuestion?.images ?? []), ...objectUrls])
+    }
+    e.target.value = ''
+  }
+
+  const onRemoveImage = async (index: number) => {
+    if (newQuestion?.images) {
+      URL.revokeObjectURL(newQuestion.images[index])
+      try {
+        const resultAction = await dispatch(removeImage(index))
+        unwrapResult(resultAction)
+      } catch (err) {
+        console.error('Failed to remove image: ', err)
+      }
+    }
+  }
+
   return (
     <>
       <DefaultLayout middleware={middleware}>
@@ -30,12 +78,78 @@ const CreateQuestion: NextPage = () => {
       </DefaultLayout>
       <Grid container justifyContent='center' marginBottom={3}>
         <Grid item xs={12} md={8} marginBottom={2}>
-          <QuestionInputArea
-            text={newQuestion?.text ?? ''}
-            images={newQuestion?.images ?? []}
-            textChangeHandler={onChangeQuestionContent}
-            imagesChangeHandler={onChangeQuestionImages}
-          />
+          <Box position='relative' width='100%'>
+            <TextareaAutosize
+              minRows={5}
+              maxRows={100}
+              placeholder='ここに質問を入力してください。右下のアイコンをクリックすると画像を追加できます。画像は最大5枚までアップロードできます。'
+              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                onChangeQuestionContent(e.target.value)
+              }
+              value={newQuestion?.text ?? ''}
+              style={{
+                fontSize: 16,
+                padding: 10,
+                width: '100%',
+                borderRadius: 30,
+              }}
+            />
+          </Box>
+          <Box textAlign='right'>
+            <CameraAltOutlinedIcon
+              onClick={() => cameraRef.current?.click()}
+              style={{
+                color: 'white',
+                fontSize: 80,
+              }}
+            />
+            <input
+              type='file'
+              hidden
+              ref={cameraRef}
+              onChange={onFileChange}
+              accept='image/*,.heic,.heif'
+              capture='environment'
+              multiple
+            />
+            <ImageOutlinedIcon
+              onClick={() => inputRef.current?.click()}
+              style={{
+                color: 'white',
+                fontSize: 80,
+              }}
+            />
+            <input
+              type='file'
+              hidden
+              ref={inputRef}
+              onChange={onFileChange}
+              accept='image/*,.heic,.heif'
+              multiple
+            />
+          </Box>
+          {newQuestion?.images?.map((src, index) => (
+            <Box
+              key={index}
+              width='100%'
+              textAlign='center'
+              position='relative'
+              marginTop={4}
+            >
+              <IconButton
+                onClick={() => onRemoveImage(index)}
+                style={{ position: 'absolute', top: -40, left: 0 }}
+                sx={{ color: 'black' }}
+              >
+                <CloseIcon style={{ fontSize: 30 }} />
+              </IconButton>
+              <img
+                src={src}
+                alt='画像のプレビュー'
+                style={{ maxWidth: '100%' }}
+              />
+            </Box>
+          ))}
         </Grid>
         <Grid item container justifyContent='center' spacing={1}>
           <Grid item xs={6} md={2}>
