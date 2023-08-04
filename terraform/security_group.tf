0c526eb3706112c7b224da9d1f4156b6628865a0
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-04 22:33:49 +0900

    ECSとRDSのセキュリティグループとルール作成 & ALBのセキュリティグループ作成

---
+++ terraform/security_group.tf
@@ -0,0 +1,81 @@
+# ---------------------------------------------
+# Security Group
+# ---------------------------------------------
+### ALB Security Group
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project}-${var.environment}-alb-sg"
+  description = "Security Group for ALB"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-alb-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+### Ingress rule for ALB
+resource "aws_security_group_rule" "alb_ingress" {
+
+}
+
+### Egress rule for ALB
+resource "aws_security_group_rule" "alb_egress" {
+
+}
+
+### ECS Security Group
+resource "aws_security_group" "ecs_sg" {
+  name        = "${var.project}-${var.environment}-ecs-sg"
+  description = "Security Group for ECS"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-ecs-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+### Ingress rule for ECS
+resource "aws_security_group_rule" "ecs_ingress" {
+  security_group_id        = aws_security_group.ecs_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = aws_security_group.alb_sg.id
+}
+
+### Egress rule for ECS
+resource "aws_security_group_rule" "ecs_egress" {
+  security_group_id = aws_security_group.ecs_sg.id
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+### RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project}-${var.environment}-rds-sg"
+  description = "Security Group for RDS"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-rds-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+### Ingress rule for RDS
+resource "aws_security_group_rule" "db_ingress_mysql_from_ecs" {
+  security_group_id        = aws_security_group.rds_sg.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 3306
+  to_port                  = 3306
+  source_security_group_id = aws_security_group.ecs_sg.id
+}
