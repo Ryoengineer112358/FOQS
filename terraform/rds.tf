59818d665027d8a54e34448b3b14478920fd6143
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-05 20:20:15 +0900

    RDSのサブネットグループ作成

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,8 +1,8 @@
 # ---------------------------------------------
 # RDS parameter group
 # ---------------------------------------------
-resource "aws_db_parameter_group" "mysql_parametergroup" {
-  name   = "${var.project}-${var.environment}-mysql-parametergroup"
+resource "aws_db_parameter_group" "mysql_parameter_group" {
+  name   = "${var.project}-${var.environment}-mysql-parameter-group"
   family = "mysql8.0"
 
   parameter {
@@ -13,5 +13,22 @@ resource "aws_db_parameter_group" "mysql_parametergroup" {
   parameter {
     name  = "character_set_server"
     value = "utf8mb4"
+  }
+}
+
+# ---------------------------------------------
+# RDS subnet group
+# ---------------------------------------------
+resource "aws_db_subnet_group" "mysql_subnet_group" {
+  name = "${var.project}-${var.environment}-mysql-subnet-group"
+  subnet_ids = [
+    aws_subnet.private_subnet_1a.id,
+    aws_subnet.private_subnet_1c.id
+  ]
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-subnet-group"
+    Project = var.project
+    Env     = var.environment
   }
 }
