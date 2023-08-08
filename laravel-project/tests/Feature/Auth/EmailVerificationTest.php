01d30739549225211da924e9199a9f27d41ea97e
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-08 19:58:13 +0900

    メールアドレス認証のFeatureテスト実装

--- laravel-project/tests/Feature/Auth/EmailVerificationTest.php
+++ laravel-project/tests/Feature/Auth/EmailVerificationTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
-use App\Providers\RouteServiceProvider;
+use App\Models\Student;
+use App\Models\Tutor;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
@@ -14,9 +14,9 @@ class EmailVerificationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_email_can_be_verified()
+    public function test_students_can_verify_email()
     {
-        $user = User::factory()->create([
+        $student = Student::factory()->create([
             'email_verified_at' => null,
         ]);
 
@@ -25,34 +25,68 @@ class EmailVerificationTest extends TestCase
         $verificationUrl = URL::temporarySignedRoute(
             'verification.verify',
             now()->addMinutes(60),
-            ['id' => $user->id, 'hash' => sha1($user->email)]
+            ['id' => $student->id, 'hash' => sha1($student->email)]
         );
 
-        $response = $this->actingAs($user)->get($verificationUrl);
+        $response = $this->actingAs($student)->get($verificationUrl);
 
         Event::assertDispatched(Verified::class);
-        $this->assertTrue($user->fresh()->hasVerifiedEmail());
-        $response->assertRedirect(
-            config('app.frontend_url') .
-                RouteServiceProvider::HOME .
-                '?verified=1'
-        );
+        $this->assertTrue($student->fresh()->hasVerifiedEmail());
+        $response->assertRedirect(config('app.frontend_url') . '/student');
     }
 
-    public function test_email_is_not_verified_with_invalid_hash()
+    public function test_students_cannot_verify_email_with_invalid_hash()
     {
-        $user = User::factory()->create([
+        $student = Student::factory()->create([
             'email_verified_at' => null,
         ]);
 
         $verificationUrl = URL::temporarySignedRoute(
             'verification.verify',
             now()->addMinutes(60),
-            ['id' => $user->id, 'hash' => sha1('wrong-email')]
+            ['id' => $student->id, 'hash' => sha1('wrong-email')]
         );
 
-        $this->actingAs($user)->get($verificationUrl);
+        $this->actingAs($student)->get($verificationUrl);
 
-        $this->assertFalse($user->fresh()->hasVerifiedEmail());
+        $this->assertFalse($student->fresh()->hasVerifiedEmail());
+    }
+
+    public function test_tutors_can_verify_email()
+    {
+        $tutor = Tutor::factory()->create([
+            'email_verified_at' => null,
+        ]);
+
+        Event::fake();
+
+        $verificationUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addMinutes(60),
+            ['id' => $tutor->id, 'hash' => sha1($tutor->email)]
+        );
+
+        $response = $this->actingAs($tutor)->get($verificationUrl);
+
+        Event::assertDispatched(Verified::class);
+        $this->assertTrue($tutor->fresh()->hasVerifiedEmail());
+        $response->assertRedirect(config('app.frontend_url') . '/tutor');
+    }
+
+    public function test_tutors_cannot_verify_email_with_invalid_hash()
+    {
+        $tutor = Tutor::factory()->create([
+            'email_verified_at' => null,
+        ]);
+
+        $verificationUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addMinutes(60),
+            ['id' => $tutor->id, 'hash' => sha1('wrong-email')]
+        );
+
+        $this->actingAs($tutor)->get($verificationUrl);
+
+        $this->assertFalse($tutor->fresh()->hasVerifiedEmail());
     }
 }
