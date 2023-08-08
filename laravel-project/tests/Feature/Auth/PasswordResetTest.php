6ab6ffe8253d6af68f146135549d407df974662b
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-08 19:58:48 +0900

    パスワードリセット処理のFeatureテスト実装

--- laravel-project/tests/Feature/Auth/PasswordResetTest.php
+++ laravel-project/tests/Feature/Auth/PasswordResetTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
-use Illuminate\Auth\Notifications\ResetPassword;
+use App\Models\Student;
+use App\Models\Tutor;
+use App\Notifications\ResetPasswordNotification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
@@ -12,33 +13,70 @@ class PasswordResetTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_reset_password_link_can_be_requested()
+    public function test_students_can_request_reset_password_link()
     {
         Notification::fake();
 
-        $user = User::factory()->create();
+        $student = Student::factory()->create();
 
-        $this->post('/forgot-password', ['email' => $user->email]);
+        $this->post('/forgot-password', ['email' => $student->email, 'user_type' => 'student']);
 
-        Notification::assertSentTo($user, ResetPassword::class);
+        Notification::assertSentTo($student, ResetPasswordNotification::class);
     }
 
-    public function test_password_can_be_reset_with_valid_token()
+    public function test_tutors_can_request_reset_password_link()
     {
         Notification::fake();
 
-        $user = User::factory()->create();
+        $tutor = Tutor::factory()->create();
 
-        $this->post('/forgot-password', ['email' => $user->email]);
+        $this->post('/forgot-password', ['email' => $tutor->email, 'user_type' => 'tutor']);
 
-        Notification::assertSentTo($user, ResetPassword::class, function (
+        Notification::assertSentTo($tutor, ResetPasswordNotification::class);
+    }
+
+    public function test_students_can_reset_password_with_valid_token()
+    {
+        Notification::fake();
+
+        $student = Student::factory()->create();
+
+        $this->post('/forgot-password', ['email' => $student->email, 'user_type' => 'student']);
+
+        Notification::assertSentTo($student, ResetPasswordNotification::class, function (
             $notification
-        ) use ($user) {
+        ) use ($student) {
             $response = $this->post('/reset-password', [
                 'token' => $notification->token,
-                'email' => $user->email,
-                'password' => 'password',
-                'password_confirmation' => 'password',
+                'email' => $student->email,
+                'password' => 'new_password',
+                'password_confirmation' => 'new_password',
+                'user_type' => 'student',
+            ]);
+
+            $response->assertSessionHasNoErrors();
+
+            return true;
+        });
+    }
+
+    public function test_tutors_can_reset_password_with_valid_token()
+    {
+        Notification::fake();
+
+        $tutor = Tutor::factory()->create();
+
+        $this->post('/forgot-password', ['email' => $tutor->email, 'user_type' => 'tutor']);
+
+        Notification::assertSentTo($tutor, ResetPasswordNotification::class, function (
+            $notification
+        ) use ($tutor) {
+            $response = $this->post('/reset-password', [
+                'token' => $notification->token,
+                'email' => $tutor->email,
+                'password' => 'new_password',
+                'password_confirmation' => 'new_password',
+                'user_type' => 'tutor',
             ]);
 
             $response->assertSessionHasNoErrors();
