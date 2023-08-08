15193516585d6704da19c8561536b62c7df98bef
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-08 19:59:21 +0900

    新規ユーザー登録処理のFeatureテスト実装

--- laravel-project/tests/Feature/Auth/RegistrationTest.php
+++ laravel-project/tests/Feature/Auth/RegistrationTest.php
@@ -9,16 +9,42 @@ class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_new_users_can_register()
+    public function test_new_students_can_register()
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'user_type' => 'student',
+            'last_name' => 'Test Student',
+            'first_name' => 'Test Student',
+            'email' => 'student@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'birth_date' => '2006-08-21',
+            'gender' => 1,
+            'high_school' => 'Test High School',
+            'first_choice_university' => 'Test University',
+            'first_choice_faculty' => 'Test Faculty',
         ]);
 
-        $this->assertAuthenticated();
-        $response->assertNoContent();
+        $this->assertAuthenticated('students');
+        $response->assertStatus(201);
+    }
+
+    public function test_new_tutors_can_register()
+    {
+        $response = $this->post('/register', [
+            'user_type' => 'tutor',
+            'last_name' => 'Test Tutor',
+            'first_name' => 'Test Tutor',
+            'email' => 'tutor@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'birth_date' => '1998-12-05',
+            'gender' => 2,
+            'university' => 'Test University',
+            'faculty' => 'Test Faculty',
+        ]);
+
+        $this->assertAuthenticated('tutors');
+        $response->assertStatus(201);
     }
 }
