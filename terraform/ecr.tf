4bac45d21f6e31e4144c130a48028a67e2f10583
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-14 00:56:54 +0900

    ECRリポジトリ作成

---
+++ terraform/ecr.tf
@@ -0,0 +1,17 @@
+# ---------------------------------------------
+# ECR Repository
+# ---------------------------------------------
+### ECR Repository for Laravel App
+resource "aws_ecr_repository" "laravel_app_repository" {
+  name = "${var.project}-${var.environment}-laravel-app-repository"
+}
+
+### ECR Repository for Nginx
+resource "aws_ecr_repository" "nginx_repository" {
+  name = "${var.project}-${var.environment}-nginx-repository"
+}
+
+### ECR Repository for Next.js frontend
+resource "aws_ecr_repository" "nextjs_frontend_repository" {
+  name = "${var.project}-${var.environment}-nextjs-frontend-repository"
+}
