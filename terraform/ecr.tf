8d3307e8f9b15b20a21691a45150ae699efa650d
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-14 16:56:17 +0900

    不要なECRリポジトリを削除

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,11 +6,6 @@ resource "aws_ecr_repository" "laravel_app_repository" {
   name = "${var.project}-${var.environment}-laravel-app-repository"
 }
 
-### ECR Repository for Nginx
-resource "aws_ecr_repository" "nginx_repository" {
-  name = "${var.project}-${var.environment}-nginx-repository"
-}
-
 ### ECR Repository for Next.js frontend
 resource "aws_ecr_repository" "nextjs_frontend_repository" {
   name = "${var.project}-${var.environment}-nextjs-frontend-repository"
