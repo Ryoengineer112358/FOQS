7476b65cbfd1b1a286e61dd52e82696df61cf68c
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-14 15:51:43 +0900

    MySQLのバージョン変更

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -43,7 +43,7 @@ resource "random_string" "db_password" {
 
 resource "aws_db_instance" "mysql" {
   engine         = "mysql"
-  engine_version = "8.0.34"
+  engine_version = "8.0.28"
 
   identifier = "${var.project}-${var.environment}-mysql"
 
