928e3bee3bb8657326cf8cb93413c66ce687edf4
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-15 09:56:22 +0900

    ECSのCodeDeploy用のIAMロールとポリシーアタッチメントを追加

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -44,3 +44,32 @@ resource "aws_iam_role" "ecs_task_role" {
     ]
   })
 }
+
+# ---------------------------------------------
+# IAM Role for CodeDeploy
+# ---------------------------------------------
+
+### CodeDeploy Role for ECS
+resource "aws_iam_role" "ecs_code_deploy_role" {
+  name = "${var.project}-${var.environment}-ecs-codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "codedeploy.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+### Attach the necessary policy for CodeDeploy with ECS
+resource "aws_iam_role_policy_attachment" "ecs_code_deploy_attachment" {
+  role       = aws_iam_role.ecs_code_deploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECSLimited"
+}
