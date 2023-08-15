74d5e3cd43929a258aa8e4a59450dd47e1731944
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-15 09:52:09 +0900

    ALB用のHTTPS証明書とそのDNS検証リソースを作成

---
+++ terraform/acm.tf
@@ -0,0 +1,24 @@
+# -------------------------------------------
+# AWS Certificate Manager
+# -------------------------------------------
+### HTTPS Certificate for ALB
+resource "aws_acm_certificate" "tokyo_cert" {
+  domain_name       = data.aws_route53_zone.hostzone.name
+  validation_method = "DNS"
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-tokyo-cert"
+    Project = var.project
+    Env     = var.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+### DNS Validation for HTTPS Certificate
+resource "aws_acm_certificate_validation" "tokyo_cert_validation" {
+  certificate_arn         = aws_acm_certificate.tokyo_cert.arn
+  validation_record_fqdns = [for r in aws_route53_record.route53_acm_dns_resolve : r.fqdn]
+}
