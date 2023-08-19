35ecc5bae2c672d7eae6f16d9cb5e25bba1fbcd3
Ryoengineer112358 <ryoengineer112358@gmail.com>
2023-08-19 23:55:17 +0900

    Nginx用のECRリポジトリ作成

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -10,3 +10,8 @@ resource "aws_ecr_repository" "laravel_app_repository" {
 resource "aws_ecr_repository" "nextjs_frontend_repository" {
   name = "${var.project}-${var.environment}-nextjs-frontend-repository"
 }
+
+### ECR Repository for Nginx
+resource "aws_ecr_repository" "nginx_repository" {
+  name = "${var.project}-${var.environment}-nginx-repository"
+}
